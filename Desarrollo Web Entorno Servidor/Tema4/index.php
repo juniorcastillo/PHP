cee34c15d2ecb9f5c608f1067602097c1cc501cf
junior <juniormiguelcastillosantana@gmail.com>
2016-10-06 08:24:07 +0200

    Actualizar

--- Desarrollo Web Entorno Servidor/Tema4/index.php
+++ Desarrollo Web Entorno Servidor/Tema4/index.php
@@ -11,7 +11,35 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        
+        if (isset($_REQUEST['digito'])) {
+           $oportunidades = $_REQUEST['oportunidades'];
+          
+           if($oportunidades<4){
+            $pega = $_REQUEST['pega'];
+            $pega = $pega . $_REQUEST['digito'];
+
+            echo $pega;
+           }
+        }
         ?>
+        <table>
+            <tr>
+                <td><a href="index.php?digito=1&oportunidades=<?= $oportunidades=1; ?>&pega=<?= $pega ?>"><button>1</button></a></td>
+                <td><a href="index.php?digito=2&oportunidades<?= $oportunidades=1;?>&pega=<?=$pega?>"><button>2</button></a></td>
+                <td><a href="index.php?digito=3&oportunidades<?= $oportunidades=1; ?>&pega=<?=$pega?>"><button>3</button></a></td></tr>
+            <tr>
+                <td><a href="index.php?digito=4&oportunidades<?= $oportunidades=1; ?>&pega=<?=$pega?>"><button>4</button></a></td>
+                <td><a href="index.php?digito=5&oportunidades<?= $oportunidades=1;?>&pega=<?=$pega?>"><button>5</button></a></td>
+                <td><a href="index.php?digito=6&oportunidades<?= $oportunidades=1;?>&pega=<?=$pega?>"><button>6</button></a></td></tr>
+
+            <tr>
+                <td><a href="index.php?digito=7&oportunidades<?= $oportunidades=1; ?>&pega=<?=$pega?>"><button>7</button></a></td>
+                <td><a href="index.php?digito=8&oportunidades<?= $oportunidades=1; ?>&pega=<?=$pega?>"><button>8</button></a></td>
+                <td><a href="index.php?digito=9&oportunidades<?= $oportunidades=1;?>&pega=<?=$pega?>"><button>9</button></a></td></tr>
+        </table>
+
+
+
     </body>
 </html>
