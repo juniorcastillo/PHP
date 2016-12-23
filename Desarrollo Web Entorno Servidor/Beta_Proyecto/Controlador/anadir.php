624c4f1abd12c220062b9ae22bfa0354040af807
juniorcastillo <juniormiguelcastillosantana@gmail.com>
2016-12-23 14:53:33 +0100

    Practica MVC

--- Desarrollo Web Entorno Servidor/Beta_Proyecto/Controlador/anadir.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ALL ^ E_NOTICE);//Que me notifique de todos los errores menso de la notice
-require_once '../Modelo/Contenido.php';
-
-$inmueble_Anadir = new Contenido($_POST[idNuevo],$_POST[fechaAltaNuevo],$_POST[tipoNuevo],$_POST[operacionNuevo],$_POST[provinciaNuevo],$_POST[superficieNuevo],$_POST[precioNuevo],$_POST[vendedorNuevo],$_POST[imagenNuevo]);
-$inmueble_Anadir->insert();
-
-echo" Este es el id ". $_POST[superficieNuevo] ;
-
